d9ffe418adcbcd612f5ece198e76ff6aef182036
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-09-27 16:24:47 +0200

    added processor structure

---
+++ src/main/java/de/netbeacon/xenia/backend/processor/RequestProcessor.java
@@ -0,0 +1,70 @@
+/*
+ *     Copyright 2020 Horstexplorer @ https://www.netbeacon.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.netbeacon.xenia.backend.processor;
+
+import de.netbeacon.utils.sql.connectionpool.SQLConnectionPool;
+import de.netbeacon.xenia.backend.clients.objects.Client;
+import io.javalin.http.Context;
+
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+
+public abstract class RequestProcessor {
+
+    private final String identifier;
+    private final SQLConnectionPool sqlConnectionPool;
+    private final ConcurrentHashMap<String, RequestProcessor> processorHashMap = new ConcurrentHashMap<>();
+
+    public RequestProcessor(String identifier, SQLConnectionPool sqlConnectionPool){
+        this.identifier = identifier;
+        this.sqlConnectionPool = sqlConnectionPool;
+    }
+
+    public RequestProcessor(String identifier, SQLConnectionPool sqlConnectionPool, RequestProcessor...requestProcessors){
+        this.identifier = identifier;
+        this.sqlConnectionPool = sqlConnectionPool;
+        Arrays.stream(requestProcessors).forEach(p->processorHashMap.put(p.getIdentifier(), p));
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public SQLConnectionPool getSqlConnectionPool(){
+        return sqlConnectionPool;
+    }
+
+    public RequestProcessor next(String identifier){
+        return processorHashMap.get(identifier);
+    }
+
+    public void get(Client client, Context ctx){
+        ctx.result("Not Implemented");
+    }
+
+    public void put(Client client, Context ctx){
+        ctx.result("Not Implemented");
+    }
+
+    public void post(Client client, Context ctx){
+        ctx.result("Not Implemented");
+    }
+
+    public void delete(Client client, Context ctx){
+        ctx.result("Not Implemented");
+    }
+}
---
+++ src/main/java/de/netbeacon/xenia/backend/processor/root/Root.java
@@ -0,0 +1,36 @@
+/*
+ *     Copyright 2020 Horstexplorer @ https://www.netbeacon.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.netbeacon.xenia.backend.processor.root;
+
+import de.netbeacon.utils.sql.connectionpool.SQLConnectionPool;
+import de.netbeacon.xenia.backend.processor.RequestProcessor;
+import de.netbeacon.xenia.backend.processor.root.auth.AuthRoot;
+import de.netbeacon.xenia.backend.processor.root.data.DataRoot;
+import de.netbeacon.xenia.backend.processor.root.info.InfoRoot;
+import de.netbeacon.xenia.backend.processor.root.setup.SetupRoot;
+
+public class Root extends RequestProcessor {
+
+    public Root(SQLConnectionPool sqlConnectionPool) {
+        super("", sqlConnectionPool,
+                new AuthRoot(sqlConnectionPool),
+                new SetupRoot(sqlConnectionPool),
+                new InfoRoot(sqlConnectionPool),
+                new DataRoot(sqlConnectionPool)
+        );
+    }
+}
