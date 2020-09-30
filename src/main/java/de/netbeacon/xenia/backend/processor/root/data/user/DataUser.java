549d00e9b9c52839fe8d05ad7554bdd7782aeb08
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-09-30 21:43:26 +0200

    updated actions

--- src/main/java/de/netbeacon/xenia/backend/processor/root/data/user/DataUser.java
+++ src/main/java/de/netbeacon/xenia/backend/processor/root/data/user/DataUser.java
@@ -19,7 +19,12 @@ package de.netbeacon.xenia.backend.processor.root.data.user;
 import de.netbeacon.utils.sql.connectionpool.SQLConnectionPool;
 import de.netbeacon.xenia.backend.clients.objects.Client;
 import de.netbeacon.xenia.backend.processor.RequestProcessor;
+import de.netbeacon.xenia.joop.Tables;
+import de.netbeacon.xenia.joop.tables.records.UsersRecord;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import org.json.JSONObject;
+
 
 public class DataUser extends RequestProcessor {
 
@@ -29,21 +34,94 @@ public class DataUser extends RequestProcessor {
 
     @Override
     public void get(Client client, Context ctx) {
-        super.get(client, ctx);
+        // get user data
+        try(var con = getSqlConnectionPool().getConnection(); var context = getSqlConnectionPool().getContext(con)){
+            long userid = Long.parseLong(ctx.pathParam("userid"));
+            UsersRecord usersRecord = context.selectFrom(Tables.USERS).where(Tables.USERS.USER_ID.eq(userid)).fetch().get(0);
+            // build a nice fluffy json
+            JSONObject jsonObject = new JSONObject()
+                    .put("userId", usersRecord.getUserId())
+                    .put("creationTime", usersRecord.getCreationTimestamp())
+                    .put("internalRole", usersRecord.getInternalRole())
+                    .put("preferredLanguage", usersRecord.getPreferredLanguage());
+            // return those values
+            ctx.status(200);
+            ctx.header("Content-Type", "application/json");
+            ctx.result(jsonObject.toString());
+        }catch (Exception e) {
+            // log properly
+            throw new BadRequestResponse();
+        }
     }
 
     @Override
     public void put(Client client, Context ctx) {
-        super.get(client, ctx);
+        // update user data
+        try(var con = getSqlConnectionPool().getConnection(); var context = getSqlConnectionPool().getContext(con)){
+            long userid = Long.parseLong(ctx.pathParam("userid"));
+            // get the current user data
+            UsersRecord usersRecord = context.selectFrom(Tables.USERS).where(Tables.USERS.USER_ID.eq(userid)).fetch().get(0);
+            // get the json body
+            JSONObject newClientObject = new JSONObject(ctx.body());
+            // check if data matches
+            if(newClientObject.getLong("userId") != usersRecord.getUserId()){
+                throw new BadRequestResponse("Invalid Data");
+            }
+            // update data
+            usersRecord.setInternalRole(newClientObject.getString("internalRole"));
+            usersRecord.setPreferredLanguage(newClientObject.getString("preferredLanguage"));
+            context.executeUpdate(usersRecord);
+            // build a nice fluffy json
+            JSONObject clientObject = new JSONObject()
+                    .put("userId", usersRecord.getUserId())
+                    .put("creationTime", usersRecord.getCreationTimestamp())
+                    .put("internalRole", usersRecord.getInternalRole())
+                    .put("preferredLanguage", usersRecord.getPreferredLanguage());
+            // return those values
+            ctx.status(200);
+            ctx.header("Content-Type", "application/json");
+            ctx.result(clientObject.toString());
+        }catch (Exception e) {
+            // log properly
+            throw new BadRequestResponse();
+        }
     }
 
     @Override
     public void post(Client client, Context ctx) {
-        super.get(client, ctx);
+        // create new
+        try(var con = getSqlConnectionPool().getConnection(); var context = getSqlConnectionPool().getContext(con)){
+            long userid = Long.parseLong(ctx.pathParam("userid"));
+            // insert create new user
+            context.insertInto(Tables.USERS, Tables.USERS.USER_ID).values(userid).execute();
+            // fetch this user
+            UsersRecord usersRecord = context.selectFrom(Tables.USERS).where(Tables.USERS.USER_ID.eq(userid)).fetch().get(0);
+            // build a nice fluffy json
+            JSONObject jsonObject = new JSONObject()
+                    .put("userId", usersRecord.getUserId())
+                    .put("creationTime", usersRecord.getCreationTimestamp())
+                    .put("internalRole", usersRecord.getInternalRole())
+                    .put("preferredLanguage", usersRecord.getPreferredLanguage());
+            // return those values
+            ctx.status(202);
+            ctx.header("Content-Type", "application/json");
+            ctx.result(jsonObject.toString());
+        } catch (Exception e) {
+            // log properly
+            throw new BadRequestResponse();
+        }
     }
 
     @Override
     public void delete(Client client, Context ctx) {
-        super.get(client, ctx);
+        // delete user
+        try(var con = getSqlConnectionPool().getConnection(); var context = getSqlConnectionPool().getContext(con)){
+            long userid = Long.parseLong(ctx.pathParam("userid"));
+            context.deleteFrom(Tables.USERS).where(Tables.USERS.USER_ID.eq(userid)).execute();
+            ctx.status(200);
+        }catch (Exception e) {
+            // log properly
+            throw new BadRequestResponse();
+        }
     }
 }
