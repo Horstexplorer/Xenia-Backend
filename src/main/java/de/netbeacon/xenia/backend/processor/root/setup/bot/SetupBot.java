5d5bef7973486aacf591334182edbe1854e51b08
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-10-02 18:28:26 +0200

    added sql

--- src/main/java/de/netbeacon/xenia/backend/processor/root/setup/bot/SetupBot.java
+++ src/main/java/de/netbeacon/xenia/backend/processor/root/setup/bot/SetupBot.java
@@ -19,9 +19,21 @@ package de.netbeacon.xenia.backend.processor.root.setup.bot;
 import de.netbeacon.utils.sql.connectionpool.SQLConnectionPool;
 import de.netbeacon.xenia.backend.clients.objects.Client;
 import de.netbeacon.xenia.backend.processor.RequestProcessor;
-import io.javalin.http.Context;
+import de.netbeacon.xenia.joop.Tables;
+import de.netbeacon.xenia.joop.tables.records.InternalBotDataRecord;
+import de.netbeacon.xenia.joop.tables.records.InternalBotShardsRecord;
+import io.javalin.http.*;
+import org.jooq.Result;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetupBot extends RequestProcessor {
+
+    private final Logger logger = LoggerFactory.getLogger(SetupBot.class);
 
     public SetupBot(SQLConnectionPool sqlConnectionPool) {
         super("bot", sqlConnectionPool);
@@ -29,6 +41,43 @@ public class SetupBot extends RequestProcessor {
 
     @Override
     public void get(Client client, Context ctx) {
-        super.get(client, ctx);
+        try(var con = getSqlConnectionPool().getConnection(); var sqlContext = getSqlConnectionPool().getContext(con)){
+            // fetch data for this client
+            long clientId = client.getClientId();
+            Result<InternalBotDataRecord> internalBotDataRecords = sqlContext.selectFrom(Tables.INTERNAL_BOT_DATA).where(Tables.INTERNAL_BOT_DATA.CLIENT_ID.eq(clientId)).fetch();
+            if(internalBotDataRecords.isEmpty()){
+                throw new NotFoundResponse();
+            }
+            InternalBotDataRecord internalBotData = internalBotDataRecords.get(0);
+            // count shards
+            int shardsTotal = sqlContext.fetchCount(Tables.INTERNAL_BOT_SHARDS);
+            // get shards
+            Result<InternalBotShardsRecord> internalBotShardsRecords = sqlContext.selectFrom(Tables.INTERNAL_BOT_SHARDS).where(Tables.INTERNAL_BOT_SHARDS.CLIENT_ID.eq(clientId)).fetch();
+            if(internalBotShardsRecords.isEmpty()){
+                throw new InternalServerErrorResponse();
+            }
+            List<Integer> shards = new ArrayList<>();
+            for(InternalBotShardsRecord internalBotShardsRecord : internalBotShardsRecords){
+                shards.add(internalBotShardsRecord.getShardId());
+            }
+            // json
+            JSONObject jsonObject = new JSONObject()
+                    .put("clientId", internalBotData.getClientId())
+                    .put("clientName", internalBotData.getClientName())
+                    .put("clientDescription", internalBotData.getClientInfo())
+                    .put("discordToken", internalBotData.getDiscordToken())
+                    .put("shards", new JSONObject()
+                            .put("total", shardsTotal)
+                            .put("use", shards));
+            // return
+            ctx.status(200);
+            ctx.header("Content-Type", "application/json");
+            ctx.result(jsonObject.toString());
+        }catch (HttpResponseException e){
+            throw e;
+        }catch (Exception e){
+            logger.warn("An Error Occurred Processing InfoPublic#GET ", e);
+            throw new BadRequestResponse();
+        }
     }
 }
