53203ddcc7fd25647ebd827eb54966649cdf0807
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-01-25 19:34:10 +0100

    updated logging

--- src/main/java/de/netbeacon/utils/appender/DiscordWebhookAppender.java
+++ src/main/java/de/netbeacon/utils/appender/DiscordWebhookAppender.java
@@ -17,15 +17,18 @@
 package de.netbeacon.utils.appender;
 
 import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.send.WebhookEmbed;
+import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
 import club.minnced.discord.webhook.send.WebhookMessageBuilder;
 import de.netbeacon.utils.config.Config;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.Executors;
@@ -40,7 +43,7 @@ import java.util.concurrent.TimeUnit;
 public class DiscordWebhookAppender extends AppenderSkeleton {
 
     private final WebhookClient webhookClient;
-    private final Queue<LoggingEvent> eventCache = new LinkedList<>();
+    private final Queue<LogContainer> eventCache = new LinkedList<>();
     private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
     /**
@@ -56,21 +59,24 @@ public class DiscordWebhookAppender extends AppenderSkeleton {
             if(eventCache.isEmpty()){
                 return;
             }
-            StringBuilder stringBuilder = new StringBuilder().append("```");
-            while(stringBuilder.length() <= 1500){
-                if(eventCache.isEmpty()){
-                    break;
-                }
-                LoggingEvent loggingEvent = eventCache.remove();
-                stringBuilder.append("[").append(new Date(loggingEvent.timeStamp)).append("][").append(loggingEvent.getLevel()).append("] ")
-                        .append("Message: ").append(loggingEvent.getMessage())
-                        .append("\tCaused by: ").append(loggingEvent.getLocationInformation().fullInfo)
-                        .append("\n");
-            }
-            stringBuilder.append("Additional errors cached: ").append(eventCache.size()).append("\n").append("```");
             WebhookMessageBuilder webhookMessageBuilder = new WebhookMessageBuilder()
-                    .setUsername("Xenia-Backend")
-                    .setContent("**Log Report**\n"+stringBuilder.toString());
+                    .setUsername("Xenia-Backend");
+            int i = 0;
+            while(i++ < 10 && !eventCache.isEmpty()){
+                LogContainer logContainer = eventCache.remove();
+                WebhookEmbedBuilder webhookEmbedBuilder = new WebhookEmbedBuilder()
+                        .setColor(logContainer.getLevel() == Level.WARN ? Color.ORANGE.getRGB() : logContainer.getLevel() == Level.ERROR ? Color.RED.getRGB() : Color.BLACK.getRGB())
+                        .setTitle(new WebhookEmbed.EmbedTitle(logContainer.getLogger(), null))
+                        .addField(new WebhookEmbed.EmbedField(true, "Message", logContainer.getMessage()))
+                        .addField(new WebhookEmbed.EmbedField(true, "Level", logContainer.getLevel().toString()))
+                        .addField(new WebhookEmbed.EmbedField(true, "Timestamp", String.valueOf(logContainer.getTimestamp())))
+                        .setFooter(new WebhookEmbed.EmbedFooter("Additional errors cached: "+eventCache.size(), null));
+                if(logContainer.getStacktrace() != null){
+                    var st = logContainer.getStacktrace();
+                    webhookEmbedBuilder.setDescription(st.substring(0, Math.min(st.length(), 1950))+".....");
+                }
+                webhookMessageBuilder.addEmbeds(webhookEmbedBuilder.build());
+            }
             webhookClient.send(webhookMessageBuilder.build());
         }, 2, 2, TimeUnit.SECONDS);
     }
@@ -78,7 +84,7 @@ public class DiscordWebhookAppender extends AppenderSkeleton {
     @Override
     protected void append(LoggingEvent event) {
         if(event.getLevel().equals(Level.WARN) || event.getLevel().equals(Level.ERROR) || event.getLevel().equals(Level.FATAL)){
-            eventCache.add(event);
+            eventCache.add(new LogContainer(event));
         }
     }
 
@@ -92,5 +98,44 @@ public class DiscordWebhookAppender extends AppenderSkeleton {
     @Override
     public boolean requiresLayout() {
         return false;
+    }
+
+    private static class LogContainer {
+
+        private final Level level;
+        private final String logger;
+        private final String message;
+        private final long timestamp;
+        private String stacktrace;
+
+        public LogContainer(LoggingEvent loggingEvent){
+            this.level = loggingEvent.getLevel();
+            this.logger = loggingEvent.getLoggerName();
+            this.message = loggingEvent.getRenderedMessage();
+            this.timestamp = loggingEvent.getTimeStamp();
+            if(loggingEvent.getThrowableInformation() != null){
+                stacktrace = ExceptionUtils.getStackTrace(loggingEvent.getThrowableInformation().getThrowable());
+            }
+        }
+
+        public Level getLevel() {
+            return level;
+        }
+
+        public String getLogger() {
+            return logger;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public String getStacktrace() {
+            return stacktrace;
+        }
     }
 }
