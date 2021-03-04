c53d30bbde5cdb004f8de4aa8c3c275d78d444c6
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-03-04 22:53:06 +0100

    critical fixes

--- src/main/java/de/netbeacon/xenia/backend/core/Core.java
+++ src/main/java/de/netbeacon/xenia/backend/core/Core.java
@@ -557,6 +557,7 @@ public class Core {
                     })
                     .exception(HttpResponseException.class, (exception, ctx) -> {
                         Metrics.HTTP_REQUESTS.labels(ctx.path(), "error", String.valueOf(exception.getStatus())).inc();
+                        throw exception;
                     })
                     .after(ctx -> {
                         ctx.header("Server", "Xenia-Backend");
